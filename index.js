aeccbbbc69f3877e01bc35378dd00b0ab10442a7
Lee <102777780+anteaterdave@users.noreply.github.com>
2023-02-21 22:07:32 -0800

    js code practice

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 // JS Code
 console.log('Hello World');
+let name = 'David';
+console.log(name);
+
+// Names cannot be reserved keywords
+// Names should be meaningful (a,b,1,x are bad names, causes confusion)
+// Names cannot start with a number
+// Names contain a space or hyphen (-)
+// Names are case-sensitive
+
+let firstName = 'David';
+let lastName = 'Andrews';
+
+const interestRate =0.3;
+console.log(interestRate);
+
+let name= 'David'; // String Literal
+let age= 28; // Number Literal
+let isApproved = true; // Boolean Literal
+let firstName = undefined;
+let selectedColor = null;  
+
+typeof isApproved
+
+
